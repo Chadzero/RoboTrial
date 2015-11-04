d440e71c20e29681746c64ddaef8ccf7ca8ac271
Chad Ramey <ramey.chad@gmail.com>
2015-11-04 00:49:20 -0600

    Updating enum addition

--- RoboTrial/Models/RAIDUtilityDataModel.cs
+++ RoboTrial/Models/RAIDUtilityDataModel.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace RoboTrial.Models
 {
+    public enum RAIDUtilityVDOptions
+    {
+        Trial1,
+        Trial2
+    }
+      
+
+
     public class RAIDUtility
     {
         [Key]
         public int RAIDUtilityID { get; set; }
         public string Name { get; set; }
+        public string Prefix { get; set; }  // "/cx set  "
+
+        [ForeignKey("VDCommand")]
+        public int RAIDCommandID { get; set; }
         public virtual RAIDCommand VDCommand { get; set; }
-        public virtual ICollection<RAIDCommand> CtrlCommand { get; set; }
+
+        public virtual ICollection<RAIDCommand> CtrlCommands { get; set; }
 
         public class RAIDCommand
         {
